ec971389051eee447b549384cb6f1333aa5a25eb
confluced <confluced@gmail.com>
2023-04-08 22:15:28 +0800

    some

--- android.py
+++ android.py
@@ -1,4 +1,6 @@
 # can be run in termux. See https://github.com/termux/termux-app
+# can't bypass proxy, so please turn tour proxy off if it is running
+# however, other platform can bypass proxy, only android can't.
 
 import os
 
@@ -7,11 +9,9 @@ ip_addr = os.popen("ifconfig | grep \"inet \" | grep -Fv 127.0.0.1 | awk \'{prin
 
 import requests
 
-session = requests.Session()
-session.trust_env = False
-
 post_addr = 'http://p.njupt.edu.cn:801/eportal/?c=ACSetting&a=Login&protocol=http:&hostname=p.njupt.edu.cn&iTermType=1&wlanuserip=' + ip_addr + '&wlanacip=10.255.252.150&wlanacname=XL-BRAS-SR8806-X&mac=00-00-00-00-00-00&ip=' + ip_addr + '&enAdvert=0&queryACIP=0&loginMethod=1'
+
 # replace Bxxxxxxxx and YOUR_PASSWORD as your actual Student ID and password in next line.
 post_data = { 'DDDDD': ",0,Bxxxxxxxx@njxy", 'upass': "YOUR_PASSWORD" }
 
-session.post(post_addr, data = post_data)
+reauests.post(post_addr, data = post_data)
