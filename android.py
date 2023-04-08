bb743c427b45997ee839afc9a1aa095e040a6748
confluced <confluced@gmail.com>
2023-04-08 22:27:40 +0800

    r

--- android.py
+++ android.py
@@ -12,6 +12,7 @@ import requests
 post_addr = 'http://p.njupt.edu.cn:801/eportal/?c=ACSetting&a=Login&protocol=http:&hostname=p.njupt.edu.cn&iTermType=1&wlanuserip=' + ip_addr + '&wlanacip=10.255.252.150&wlanacname=XL-BRAS-SR8806-X&mac=00-00-00-00-00-00&ip=' + ip_addr + '&enAdvert=0&queryACIP=0&loginMethod=1'
 
 # replace Bxxxxxxxx and YOUR_PASSWORD as your actual Student ID and password in next line.
+# replace njxy to cmcc if you use the Chine Mobile and delete it if you use school provided network. (experimental)
 post_data = { 'DDDDD': ",0,Bxxxxxxxx@njxy", 'upass': "YOUR_PASSWORD" }
 
 reauests.post(post_addr, data = post_data)
